f5c70628f20816e87be75fefd6d2600031e99042
Masato Takahashi <masato.takahashi.2.71828@gmail.com>
2021-01-29 16:49:22 +0900

    fix hooks

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,13 @@
+import sys, time, os 
+import shutil
+import subprocess
+
+print(os.getcwd())
+
+dirs = ['./input', './submit', './intermediate']
+for d in dirs:
+  os.mkdir(d)
+
+subprocess.call(['git', 'init'])
+subprocess.call(['git', 'submodule', 'add', 'https://github.com/MasatoTakahashi/myDSUtils'])
+subprocess.call(['git', 'flow', 'init'])
