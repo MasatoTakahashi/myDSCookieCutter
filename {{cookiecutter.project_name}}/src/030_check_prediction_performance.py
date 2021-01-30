e652f50e7faea8a7c91c86ef986aabf7355049c6
Masato Takahashi <masato.takahashi.2.71828@gmail.com>
2021-01-30 23:45:22 +0900

    update

--- {{cookiecutter.project_name}}/src/030_check_prediction_performance.py
+++ {{cookiecutter.project_name}}/src/030_check_prediction_performance.py
@@ -1,1 +1,25 @@
-print("hello, {{cookiecutter.greeting_recipient}}")
+import sys, time, os, json
+import numpy as np 
+import pandas as pd 
+
+import matplotlib.pyplot as plt 
+import seaborn as sns
+
+from mlflow import log_metric, log_params, log_param, log_artifacts, log_artifact
+
+import logging
+from logging import getLogger
+logging.basicConfig(level=logging.INFO)
+
+sys.path.append('../')
+from myDSUtils.general_util import * 
+from myDSUtils.ml_general_util import * 
+from myDSUtils.mlflow_util import * 
+ts_now = get_now_str()
+
+from column_definition import * 
+from drop_columns import *
+from helper__data_load import * 
+from helper__feature_engineering_functions import * 
+from helper__make_submit_file import * 
+
--- {{cookiecutter.project_name}}/src/helper__make_submit_file.py
+++ {{cookiecutter.project_name}}/src/helper__make_submit_file.py
@@ -1,1 +1,19 @@
-print("hello, {{cookiecutter.greeting_recipient}}")
+import sys, time, os, json
+import numpy as np 
+import pandas as pd 
+
+import matplotlib.pyplot as plt 
+import seaborn as sns
+
+from mlflow import log_metric, log_params, log_param, log_artifacts, log_artifact
+
+import logging
+from logging import getLogger
+logging.basicConfig(level=logging.INFO)
+
+sys.path.append('../')
+from myDSUtils.general_util import * 
+from myDSUtils.ml_general_util import * 
+from myDSUtils.mlflow_util import * 
+ts_now = get_now_str()
+
